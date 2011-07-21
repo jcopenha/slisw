648e0e214c202335121afe5f1b2bdb8d50f2eadf
Jason Copenhaver <jcopenha@typedef.org>
2011-07-20 21:13:37 -0400

    making sure the one in the book works

---
+++ ruby/2.3.2/main.rb
@@ -0,0 +1,33 @@
+class Tree
+    attr_accessor :children, :node_name
+
+    def initialize(name, children=[])
+        @node_name = name
+        @children = children
+    end
+
+    def initialize(name, children=[])
+        @node_name = name
+        @children = children
+    end
+
+
+    def visit_all(&block)
+        visit(&block)
+        @children.each {|c| c.visit_all(&block)}
+    end
+
+    def visit(&block)
+        block.call self
+    end
+end
+
+#ruby_tree = Tree.new("Ruby",
+#                    [Tree.new("One"),
+#                     Tree.new("Two")])
+
+puts "Visiting a node"
+ruby_tree.visit {|node| puts node.node_name}
+
+puts "Visiting the whole tree"
+ruby_tree.visit_all {|node| puts node.node_name}
