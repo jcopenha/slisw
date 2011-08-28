b3cb6415e2289f627ba7194f0f1a1f7682e1a8f6
jcopenha <jcopenha@typedef.org>
2011-08-28 11:45:44 -0400

    my first prolog, and output

---
+++ prolog/4.2.1/books.pl
@@ -0,0 +1,8 @@
+wrote(dune, herbert).
+wrote(hhgttg, adams).
+wrote(children_of_dune, herbert).
+wrote(god_emperor_of_dune, herbert).
+wrote(chapterhouse_of_dune, herbert).
+wrote(foundation, asimov).
+wrote(grapes_of_wrath,stienbeck).
+
