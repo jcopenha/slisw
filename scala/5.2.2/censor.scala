d67f414c19bcf6616a875b8ac8d9896cedd6d799
jcopenha <jcopenha@typedef.org>
2011-09-25 11:53:03 -0400

    closer..

--- scala/5.2.2/censor.scala
+++ scala/5.2.2/censor.scala
@@ -1,13 +1,19 @@
 object Censor {
     val badwords = Map("Shoot" -> "Plucky", "Darn" -> "Beans")
     def censor(a:List[java.lang.String]) = {
-        val m = List()
+        var f = List[java.lang.String]()
         badwords.foreach(m => {
-            val m1 = a.map(word => word == m._1 ? m._2 : word)
-            m = m + m1
+            val m1 = a.map(word => if (word == m._1) {
+                                        m._2
+                                    } else {
+                                        word
+                                    })
+            println(m1)
+            f = f ++ m1
         })
+        f
     }
 }
 
-var a = List("Plucky", "jason", "else")
+var a = List("Shoot", "jason", "else")
 println(Censor.censor(a))
