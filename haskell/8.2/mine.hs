441a1128ebcca7cd729435c5c7b3b3e903c29b3d
jcopenha <jcopenha@typedef.org>
2011-11-19 17:44:55 -0500

    more haskell

--- haskell/8.2/mine.hs
+++ haskell/8.2/mine.hs
@@ -2,3 +2,8 @@ module Mine where
 
     allEven :: [Integer] -> [Integer]
     allEven x = [ y | y <- x, mod y 2 == 0]
+
+    myrev :: [a] -> [a]
+    myrev [] = []
+    myrev (h:t) = myrev t ++ [h]
+
